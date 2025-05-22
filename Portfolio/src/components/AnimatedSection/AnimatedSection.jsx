cfba0bfb8570cf812c471296609690ccd24a3f6b
Paulo Padilha <paulopadilha2302@gmail.com>
2025-05-22 15:36:15 -0300

    commit de algumas correções

--- Portfolio/src/components/AnimatedSection/AnimatedSection.jsx
+++ Portfolio/src/components/AnimatedSection/AnimatedSection.jsx
@@ -1,12 +1,29 @@
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 const AnimatedSection = ({ children }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize(); // chama na primeira renderização
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  if (isMobile) {
+    return <div>{children}</div>; // Sem animação no mobile
+  }
+
   return (
     <motion.div
-      initial={{ opacity: 0, y: 50 }} // Inicia invisível e 50px abaixo
-      whileInView={{ opacity: 1, y: 0 }} // Animação para visível e na posição original
-      viewport={{ once: false, amount: 0.2 }} // Dispara a animação apenas uma vez e quando 20% do elemento estiver visível
-      transition={{ duration: 1.2, ease: "easeOut" }} // Duração mais longa e curva de easing suave
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: false, amount: 0.2 }}
+      transition={{ duration: 1.2, ease: "easeOut" }}
     >
       {children}
     </motion.div>
